6b6cec0532b2334eb60d02a7319d46f92c10e885
f3xeek <filipziarko7@gmail.com>
2025-01-16 12:53:50 +0100

    adding sprites constant, scaling canvas based on grid size

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ window.onload = function () {
     const ctx = canvas.getContext("2d");
     const N = 19;
     const SIZE = 40;
-    //                                               1 for rigth -1 for left 0 for tail
-    let snake = { head: [2, Math.round((N - 1) / 2)], turns: [[9, 10, 1], [9, 2, 1], [1, 2, 1], [1, 5, 0]], direciton: -1, axis: 'y', length: 20 }
+    const SPRITES = {head_down: [256,64], head_up:[192,0], head_left:[192,64], head_right:[256,0],apple:[0,192], body_horizontal:[64,0],
+                    body_vertical:[128,64], tail_down:[192,128],tail_up:[256,192], tail_right:[192,192],tail_left:[256,192],
+                    turn_down_left:[128,0], turn_down_right:[0,0],  turn_up_right:[0,64], turn_up_left:[128,128]}
+  
+                    //                                               [x,y,state] 1 for rigth -1 for left 0 for tail
+    let snake = { head: [4, Math.round((N - 1) / 2)], turns: [[2, 5, 0]], direciton: -1, axis: 'y', length: 20 }
+    
+    canvas.setAttribute("width", SIZE*N)
+    canvas.setAttribute("height", SIZE*N)
+
     function draw_grid() {
         ctx.beginPath();
         for (let x = SIZE; x < N * SIZE; x += SIZE) {
@@ -18,26 +26,7 @@ window.onload = function () {
     }
     function draw_snake() {
         //drawing snakes head
-        let snake_head_x = 0
-        let snake_head_y = 0
-        if (snake.direciton === -1) {
-            snake_head_y = 64
-            if (snake.axis === "x") {
-                snake_head_x = 192
-            } else {
-                snake_head_x = 256
-            }
-        } else {
-            if (snake.axis === "y") {
-                snake_head_x = 192
-            } else {
-                snake_head_x = 256
-            }
-        }
-
-        ctx.drawImage(document.getElementById("spritesheet"), snake_head_x, snake_head_y, 64, 64, snake.head[0] * SIZE, (snake.head[1]) * SIZE, SIZE, SIZE)
-        //ctx.fillRect(snake.head[0] * SIZE, (snake.head[1]) * SIZE, SIZE, SIZE)
-
+        ctx.drawImage(document.getElementById("spritesheet"), SPRITES.head_right[0],SPRITES.head_right[1], 64, 64, snake.head[0] * SIZE, (snake.head[1]) * SIZE, SIZE, SIZE)
         let current_position = [snake.head[0], snake.head[1]]
         snake.turns.forEach((turn) => {
 
