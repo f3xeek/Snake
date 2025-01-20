6391ab34512590b60ee921b2ed066afaf508abc1
f3xeek <filipziarko7@gmail.com>
2025-01-20 14:06:18 +0100

    ANIMATIONS!!

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ window.onload = function () {
     const canvas = document.getElementById("gameBoard");
     const spritesheet = document.getElementById("spritesheet");
     const ctx = canvas.getContext("2d");
-    const N = 19;
-    const SIZE = 40;
+    const N = 10;
+    const SIZE = 60;
     const SPRITES = {head_down: [256,64], head_up:[192,0], head_left:[192,64], head_right:[256,0],apple:[0,192], body_horizontal:[64,0],
                     body_vertical:[128,64], tail_down:[192,128],tail_up:[256,192], tail_right:[192,192],tail_left:[256,128],
                     turn_down_left:[128,0], turn_down_right:[0,0], turn_up_right:[0,64], turn_up_left:[128,128]}
   
                     //                                         [x,y] 
-    let snake = { head: [5, Math.round((N - 1) / 2)], turns: [[2,Math.round((N - 1) / 2)]], head_position:SPRITES.head_right, moves:[], length:4, facing:"ArrowRight", growing: false }
+    let snake = { head: [5, Math.round((N - 1) / 2)], turns: [[2,Math.round((N - 1) / 2)]], head_position:SPRITES.head_right,animationDirection:1 ,moves:[], length:4, turnfacing:null, facing:"ArrowRight",turning:false, growing: false, animationShift:-SIZE }
     let apple = [N-3 ,Math.round((N - 1) / 2)]
     let game = false
     canvas.setAttribute("width", SIZE*N)
@@ -29,8 +29,6 @@ window.onload = function () {
     function draw_snake_and_apple() {
         //drwing apple
         ctx.drawImage(spritesheet, SPRITES.apple[0],SPRITES.apple[1], 64, 64, apple[0] * SIZE, apple[1] * SIZE , SIZE, SIZE)
-        //drawing snakes head
-        ctx.drawImage(spritesheet, snake.head_position[0], snake.head_position[1] , 64, 64, snake.head[0] * SIZE, snake.head[1] * SIZE, SIZE, SIZE)
         let current_position = [snake.head[0], snake.head[1]]
         //drawing snakes body + turns
         let snakePieces = 1
@@ -44,25 +42,17 @@ window.onload = function () {
                 for (let i = length; i > 1; i--) {
                     snakePieces+=1
                     current_position[0] += direction
-                    ctx.drawImage(spritesheet,SPRITES.body_horizontal[0],SPRITES.body_horizontal[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
+                    let canvasX = current_position[0] * SIZE + snake.animationShift * -direction
+                    let canvasY = current_position[1] * SIZE
+                    ctx.drawImage(spritesheet,SPRITES.body_horizontal[0],SPRITES.body_horizontal[1],64,64, canvasX,canvasY, SIZE, SIZE)
                 }
                 snakePieces+=1
                 current_position[0] += direction
                 if(snakePieces === snake.length){
-                    if(direction===-1) ctx.drawImage(spritesheet,SPRITES.tail_left[0],SPRITES.tail_left[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
-                    else ctx.drawImage(spritesheet,SPRITES.tail_right[0],SPRITES.tail_right[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
-                }else{
-                    //drawing turns
-                    if (nextTurn){
-                        const nextDir = nextTurn[1] - current_position[1] > 0 ? 1 : -1
-                        if (direction>0){
-                            if (nextDir>0)ctx.drawImage(spritesheet,SPRITES.turn_down_left[0],SPRITES.turn_down_left[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
-                            else ctx.drawImage(spritesheet,SPRITES.turn_up_left[0],SPRITES.turn_up_left[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)   
-                        }else{
-                            if (nextDir<0)ctx.drawImage(spritesheet,SPRITES.turn_up_right[0],SPRITES.turn_up_right[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
-                            else ctx.drawImage(spritesheet,SPRITES.turn_down_right[0],SPRITES.turn_down_right[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
-                        }
-                    }
+                    let canvasX = current_position[0] * SIZE + snake.animationShift * -direction
+                    let canvasY = current_position[1] * SIZE
+                    if(direction===-1) ctx.drawImage(spritesheet,SPRITES.tail_left[0],SPRITES.tail_left[1],64,64,canvasX,canvasY, SIZE, SIZE)
+                    else ctx.drawImage(spritesheet,SPRITES.tail_right[0],SPRITES.tail_right[1],64,64,canvasX,canvasY, SIZE, SIZE)
                 }
             } else if  (turn[1] - current_position[1] != 0) {
                 //drawing body vertical
@@ -71,27 +61,91 @@ window.onload = function () {
                 for (let i = length; i > 1; i--) {
                     snakePieces+=1
                     current_position[1] += direction
-                    ctx.drawImage(spritesheet,SPRITES.body_vertical[0],SPRITES.body_vertical[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
+                    let canvasX = current_position[0] * SIZE 
+                    let canvasY = current_position[1] * SIZE + snake.animationShift * -direction
+                    ctx.drawImage(spritesheet,SPRITES.body_vertical[0],SPRITES.body_vertical[1],64,64,canvasX,canvasY, SIZE, SIZE)
                 }
                 snakePieces+=1
                 current_position[1] += direction
                 if(snakePieces === snake.length){
-                    if(direction===1) ctx.drawImage(spritesheet,SPRITES.tail_down[0],SPRITES.tail_down[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
-                    else ctx.drawImage(spritesheet,SPRITES.tail_up[0],SPRITES.tail_up[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
-                }else{
-                    if (nextTurn){
-                        //drawing turns
-                        const nextDir =nextTurn[0] - current_position[0] > 0 ? 1 : -1
-                        if (direction>0){
-                            if (nextDir>0)ctx.drawImage(spritesheet,SPRITES.turn_up_right[0],SPRITES.turn_up_right[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
-                            else ctx.drawImage(spritesheet,SPRITES.turn_up_left[0],SPRITES.turn_up_left[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)   
-                        }else{
-                            if (nextDir>0)ctx.drawImage(spritesheet,SPRITES.turn_down_right[0],SPRITES.turn_down_right[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
-                            else ctx.drawImage(spritesheet,SPRITES.turn_down_left[0],SPRITES.turn_down_left[1],64,64,current_position[0] * SIZE,current_position[1] * SIZE, SIZE, SIZE)
-                        }
-                    }
+                    let canvasX = current_position[0] * SIZE 
+                    let canvasY = current_position[1] * SIZE + snake.animationShift * -direction
+                    if(direction===1) ctx.drawImage(spritesheet,SPRITES.tail_down[0],SPRITES.tail_down[1],64,64,canvasX,canvasY, SIZE, SIZE)
+                    else ctx.drawImage(spritesheet,SPRITES.tail_up[0],SPRITES.tail_up[1],64,64,canvasX,canvasY, SIZE, SIZE)
                 }
             }     
+    })
+    //drawing turns
+    snake.turns.forEach((turn,index) => {
+
+        if(index != snake.turns.length-1){
+            
+            const nextTurn = snake.turns[index+1]
+            const prevTurn = snake.turns[index-1]? snake.turns[index-1] : snake.head
+            let direction = 0
+            if (turn[0] - nextTurn[0] != 0){
+                direction = (turn[0] - nextTurn[0]) > 0 ? 1 : -1
+                if (prevTurn){
+                    const prevtDir = prevTurn[1] - turn[1] > 0 ? 1 : -1
+                    if (Math.abs(prevTurn[0] - turn[0])>1){
+                        let canvasX = turn[0] * SIZE 
+                        let canvasY = turn[1] * SIZE + (SIZE+snake.animationShift) * prevtDir
+                        ctx.drawImage(spritesheet,SPRITES.body_vertical[0],SPRITES.body_vertical[1],64,64, canvasX,canvasY, SIZE, SIZE)
+                    }
+                    let canvasX = turn[0] * SIZE + snake.animationShift * direction
+                    let canvasY = turn[1] * SIZE 
+                    ctx.drawImage(spritesheet,SPRITES.body_horizontal[0],SPRITES.body_horizontal[1],64,64, canvasX,canvasY, SIZE, SIZE)
+                    ctx.clearRect(turn[0]*SIZE+1,turn[1]*SIZE+1,SIZE-2,SIZE-2)
+                    if (direction>0){
+                        if (prevtDir>0)ctx.drawImage(spritesheet,SPRITES.turn_down_left[0],SPRITES.turn_down_left[1],64,64,turn[0] * SIZE,turn[1] * SIZE, SIZE, SIZE)
+                        else ctx.drawImage(spritesheet,SPRITES.turn_up_left[0],SPRITES.turn_up_left[1],64,64,turn[0] * SIZE,turn[1] * SIZE, SIZE, SIZE)   
+                    }else{
+                        if (prevtDir<0)ctx.drawImage(spritesheet,SPRITES.turn_up_right[0],SPRITES.turn_up_right[1],64,64,turn[0] * SIZE,turn[1] * SIZE, SIZE, SIZE)
+                        else ctx.drawImage(spritesheet,SPRITES.turn_down_right[0],SPRITES.turn_down_right[1],64,64,turn[0] * SIZE,turn[1] * SIZE, SIZE, SIZE)
+                    }
+                }
+            }else if(turn[1] - nextTurn[1] != 0){
+                direction = (turn[1] - nextTurn[1]) > 0 ? 1 : -1
+                if (prevTurn){
+                    const prevtDir = prevTurn[0] - turn[0] > 0 ? 1 : -1
+                    if (Math.abs(prevTurn[1] - turn[1])>1){
+                        let canvasX = turn[0] * SIZE + (SIZE+snake.animationShift) * prevtDir
+                        let canvasY = turn[1] * SIZE 
+                        ctx.drawImage(spritesheet,SPRITES.body_horizontal[0],SPRITES.body_horizontal[1],64,64, canvasX,canvasY, SIZE, SIZE)
+                    }
+                    let canvasX = turn[0] * SIZE 
+                    let canvasY = turn[1] * SIZE + snake.animationShift * direction
+                    ctx.drawImage(spritesheet,SPRITES.body_vertical[0],SPRITES.body_vertical[1],64,64, canvasX,canvasY, SIZE, SIZE)
+                    ctx.clearRect(turn[0]*SIZE+1,turn[1]*SIZE+1,SIZE-2,SIZE-2)
+                    if (direction>0){
+                        if (prevtDir>0)ctx.drawImage(spritesheet,SPRITES.turn_up_right[0],SPRITES.turn_up_right[1],64,64,turn[0] * SIZE,turn[1] * SIZE, SIZE, SIZE)
+                        else ctx.drawImage(spritesheet,SPRITES.turn_up_left[0],SPRITES.turn_up_left[1],64,64,turn[0] * SIZE,turn[1] * SIZE, SIZE, SIZE)   
+                    }else{
+                        if (prevtDir>0)ctx.drawImage(spritesheet,SPRITES.turn_down_right[0],SPRITES.turn_down_right[1],64,64,turn[0] * SIZE,turn[1] * SIZE, SIZE, SIZE)
+                        else ctx.drawImage(spritesheet,SPRITES.turn_down_left[0],SPRITES.turn_down_left[1],64,64,turn[0] * SIZE,turn[1] * SIZE, SIZE, SIZE)
+                    }
+                }
+            }
+        }
+//drawing snakes head
+        let currentAnimationShift = ["ArrowRight", "ArrowLeft"].includes(snake.facing) ? [snake.animationShift,0] : [0,snake.animationShift]
+        let canvasX = snake.head[0] * SIZE + currentAnimationShift[0]*snake.animationDirection
+        let canvasY = snake.head[1] * SIZE + currentAnimationShift[1]*snake.animationDirection
+        if (snake.turning){
+            let angle = (snake.animationShift/SIZE)*Math.PI/2
+            if (["ArrowRight", "ArrowLeft"].includes(snake.facing)){
+                if (snake.turnfacing*snake.animationDirection>=1) angle = -angle
+            }else {
+                if (snake.turnfacing*snake.animationDirection<1) angle = - angle 
+            }
+            ctx.translate(canvasX+SIZE/2,canvasY+SIZE/2)
+            ctx.rotate(angle)
+            ctx.drawImage(spritesheet, snake.head_position[0], snake.head_position[1] , 64, 64, -SIZE/2, -SIZE/2, SIZE, SIZE)
+            ctx.rotate(-angle)
+            ctx.translate(-(canvasX+SIZE/2),-(canvasY+SIZE/2))
+        }else{
+            ctx.drawImage(spritesheet, snake.head_position[0], snake.head_position[1] , 64, 64, canvasX, canvasY, SIZE, SIZE)
+        }
     })
     }
     function AddMoveToQueue(e){
@@ -119,7 +173,7 @@ window.onload = function () {
     }
 
     function checkForBorder() {
-        return (snake.head[0]>=N || snake.head[1]>=N )
+        return (snake.head[0]>=N || snake.head[1]>=N || snake.head[0]<0 || snake.head[1]<0)
     }
 
     function gameLost(){
@@ -138,7 +192,12 @@ window.onload = function () {
     }
 
     function update() {
+        snake.animationShift+=SIZE/10
+        drawFrame()
         //skips moves untill one is valid, different axis than previosusly
+        if (snake.animationShift===0){ 
+            snake.animationShift=-SIZE
+            snake.turning= false
         while (snake.moves.length > 0 && (
             (["ArrowDown", "ArrowUp"].includes(snake.moves[0])&&["ArrowDown", "ArrowUp"].includes(snake.facing))
             ||(["ArrowLeft", "ArrowRight"].includes(snake.moves[0])&&["ArrowLeft", "ArrowRight"].includes(snake.facing))
@@ -152,26 +211,33 @@ window.onload = function () {
                 snake.head[1]+=direction
             }
         }else{
+            snake.turnfacing = snake.animationDirection
+            snake.turning = true
             snake.turns.unshift([snake.head[0], snake.head[1]])
             if (snake.moves[0] ==="ArrowDown"){
                 snake.head[1]+=1
+                snake.animationDirection = 1
                 snake.head_position = SPRITES.head_down
             }
             else if (snake.moves[0] ==="ArrowUp"){
                 snake.head[1]-=1
+                snake.animationDirection = -1
                 snake.head_position = SPRITES.head_up
             }
             else if (snake.moves[0] ==="ArrowLeft"){
                 snake.head[0]-=1
+                snake.animationDirection = -1
                 snake.head_position = SPRITES.head_left
             }
             else if (snake.moves[0] ==="ArrowRight"){
                 snake.head[0]+=1
+                snake.animationDirection = 1
                 snake.head_position = SPRITES.head_right
             }
             snake.facing = snake.moves[0]
             snake.moves.splice(0,1)
         }
+
 
         
             checkApple()
@@ -193,7 +259,7 @@ window.onload = function () {
             }
             if(checkForBorder() || checkForSnake(snake.head)) gameLost()
             else drawFrame()
-
+        }
 
     }
     function initialize() {
@@ -201,7 +267,7 @@ window.onload = function () {
         document.getElementById("body").onkeydown = (e)=>{
             if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.code)){
                 if (!game){
-                    test = setInterval(update,200)
+                    test = setInterval(update,20)
                     game=true
                 }
                 AddMoveToQueue(e)
